2538ea7f0f3d448583755e548f6a84fdbd89276c
gb <guns.blazin.7@gmail.com>
2018-08-14 08:37:46 +0300

    axisPointer / line

--- src/components/xAxis.ts
+++ src/components/xAxis.ts
@@ -1,9 +1,9 @@
 import styles from "./styles";
 import * as chartUtils from "./chartUtils";
 
-interface FormatterParams {
-  value: number;
-}
+// interface FormatterParams {
+//   value: number;
+// }
 
 export default {
   type: "time",
@@ -14,30 +14,30 @@ export default {
     formatter: function(ts: number, index: number) {
       return chartUtils.formatDateShort(ts);
     }
-  },
-  axisPointer: {
-    // value: '2016-10-7',
-    snap: true,
-    triggerOn: "mousemove", // click mousemove mousemove|click
-    type: "line", // line / shadow
-    // status: true,
-    lineStyle: {
-      type: "dotted", // solid / dashed / dotted
-      color: styles.colors.primary,
-      opacity: 0.5,
-      width: 5
-    },
-    label: {
-      show: false,
-      formatter: function(params: FormatterParams) {
-        return params.value;
-      },
-      backgroundColor: "#004E52"
-    },
-    handle: {
-      show: true,
-      size: 0
-      // color: '#004E52'
-    }
   }
+  // axisPointer: {
+  //   // value: '2016-10-7',
+  //   snap: true,
+  //   triggerOn: "mousemove", // click mousemove mousemove|click
+  //   type: "line", // line / shadow
+  //   // status: true,
+  //   lineStyle: {
+  //     type: "dotted", // solid / dashed / dotted
+  //     color: styles.colors.primary,
+  //     opacity: 0.5,
+  //     width: 5
+  //   },
+  //   label: {
+  //     show: false,
+  //     formatter: function(params: FormatterParams) {
+  //       return params.value;
+  //     },
+  //     backgroundColor: "#004E52"
+  //   },
+  //   handle: {
+  //     show: true,
+  //     size: 0
+  //     // color: '#004E52'
+  //   }
+  // }
 };
