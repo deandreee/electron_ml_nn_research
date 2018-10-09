3c6b32d9dcd32ef159453caf259a6bd7b0f7f9e1
gb <guns.blazin.7@gmail.com>
2018-10-09 18:54:09 +0300

    recall / fscore

--- src/strat/mlEvaluate.ts
+++ src/strat/mlEvaluate.ts
@@ -1,13 +1,19 @@
 import { NumberMap } from "./mlUtils";
 import { sum } from "lodash";
 
+// * Precision/Specificity: how many selected instances are relevant.
+// * Recall/Sensitivity: how many relevant instances are selected.
+// * F1 score: harmonic mean of precision and recall.
+
 export const evaluateResults = (uniqueLabels: number[], input: number[], output: number[]) => {
   let truePositives: NumberMap = {};
   let falsePositives: NumberMap = {};
+  let falseNegatives: NumberMap = {};
 
   for (let x of uniqueLabels) {
     truePositives[x] = 0;
     falsePositives[x] = 0;
+    falseNegatives[x] = 0;
   }
 
   for (let i = 0; i < input.length; i++) {
@@ -16,23 +22,31 @@ export const evaluateResults = (uniqueLabels: number[], input: number[], output:
     if (input[i] === output[i]) {
       truePositives[label]++;
     } else {
-      falsePositives[output[i]]++;
+      falsePositives[output[i]]++; // is, but but have not been
+      falseNegatives[input[i]]++; // should have been, but is not
     }
   }
 
   let precision: NumberMap = {};
+  let recall: NumberMap = {};
   for (let x of uniqueLabels) {
     const px = truePositives[x] / (truePositives[x] + falsePositives[x]);
+    const rx = truePositives[x] / (truePositives[x] + falseNegatives[x]);
     precision[x] = !isNaN(px) ? px : 0;
+    recall[x] = !isNaN(rx) ? rx : 0;
   }
 
   console.log("preision", precision);
 
   const precisionTotal = sum(objToArr(precision)) / uniqueLabels.length;
+  const recallTotal = sum(objToArr(recall)) / uniqueLabels.length;
+  const fScore = (2 * precisionTotal * recallTotal) / (precisionTotal + recallTotal);
 
   console.log("PRECISION_TOTAL", precisionTotal);
+  console.log("RECALL_TOTAL", recallTotal);
+  console.log("F_SCORE", fScore);
 
-  return { precision, precisionTotal };
+  return { fScore, precision, precisionTotal, recall, recallTotal };
 };
 
 const objToArr = (o: any) => {
