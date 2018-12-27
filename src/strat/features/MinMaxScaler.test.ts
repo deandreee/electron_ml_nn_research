a4d5bb7ea1bd8de19db4bb1d6cc6c4d0a0d67109
gb <guns.blazin.7@gmail.com>
2018-12-27 14:12:58 +0200

    scale(Initial) + tests

--- src/strat/features/MinMaxScaler.test.ts
+++ src/strat/features/MinMaxScaler.test.ts
@@ -1,40 +1,172 @@
 import { MinMaxScaler } from "./MinMaxScaler";
 import { FeatureSplit } from ".";
+import { round1 } from "../utils";
 
-test("featureSplit | single", () => {
-  const featureSplit: FeatureSplit = { name: "rsi_combo", fn: x => [x.ind.rsi30x10] };
-  const minMaxScaler = new MinMaxScaler(featureSplit);
-  const indicators = minMaxScaler.getIndicatorNames();
-  expect(indicators).toEqual(["rsi30x10"]);
+describe("featureSplit", () => {
+  test("getIndicatorNames | single", () => {
+    const featureSplit: FeatureSplit = { name: "rsi_combo", fn: x => [x.ind.rsi30x10] };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+    const indicators = minMaxScaler.getIndicatorNames();
+    expect(indicators).toEqual(["rsi30x10"]);
+  });
+
+  test("getIndicatorNames | multiple", () => {
+    const featureSplit: FeatureSplit = {
+      name: "rsi_combo_macd",
+      fn: x => [x.ind.rsi30x10, x.ind.rsi60x10, x.ind.rsi120x10, x.ind.rsi240x10, x.ind.rsi480x10]
+    };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+    const indicators = minMaxScaler.getIndicatorNames();
+    expect(indicators).toEqual(["rsi30x10", "rsi60x10", "rsi120x10", "rsi240x10", "rsi480x10"]);
+  });
+
+  test("getIndicatorNames | with point", () => {
+    const featureSplit: FeatureSplit = { name: "rsi_combo", fn: x => [x.ind.macd30.histo] };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+    const indicators = minMaxScaler.getIndicatorNames();
+    expect(indicators).toEqual(["macd30.histo"]);
+  });
+
+  test("getIndicatorNames | comment", () => {
+    const featureSplit: FeatureSplit = {
+      name: "rsi_combo",
+      fn: x => [
+        x.ind.rsi30x10,
+        // x.ind.rsi60x10,
+        x.ind.rsi120x10
+      ]
+    };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+    const indicators = minMaxScaler.getIndicatorNames();
+    expect(indicators).toEqual(["rsi30x10", "rsi120x10"]);
+  });
+
+  test("scaleInitial | feature count too much", () => {
+    const featureSplit: FeatureSplit = {
+      name: "rsi_combo",
+      fn: x => [x.ind.rsi30x10, x.ind.rsi120x10]
+    };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+
+    const features = [[1, 2, 3]];
+    expect(() => {
+      minMaxScaler.scaleInitial(features);
+    }).toThrowError("ERROR: indicators.length (2) !== features.length (3)");
+  });
+
+  test("scaleInitial | feature count not enough", () => {
+    const featureSplit: FeatureSplit = {
+      name: "rsi_combo",
+      fn: x => [x.ind.rsi30x10, x.ind.rsi120x10]
+    };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+
+    const features = [[1]];
+    expect(() => {
+      minMaxScaler.scaleInitial(features);
+    }).toThrowError("ERROR: indicators.length (2) !== features.length (1)");
+  });
+
+  test("scaleInitial | called 2x", () => {
+    const featureSplit: FeatureSplit = {
+      name: "rsi_combo",
+      fn: x => [x.ind.rsi30x10]
+    };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+
+    const features = [[1]];
+    expect(() => {
+      minMaxScaler.scaleInitial(features);
+      minMaxScaler.scaleInitial(features);
+    }).toThrowError("ERROR: scaleInitial can only be called single time");
+  });
+
+  test("scaleInitial | rsi", () => {
+    const featureSplit: FeatureSplit = {
+      name: "rsi_combo",
+      fn: x => [x.ind.rsi30x10]
+    };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+
+    const features = [[0], [25], [50], [75], [100]];
+    const scaled = minMaxScaler.scaleInitial(features);
+
+    expect(scaled).toEqual([[-1], [-0.5], [0], [0.5], [1]]);
+  });
+
+  test("scaleInitial | macd | 0 -> 10", () => {
+    const featureSplit: FeatureSplit = {
+      name: "macd",
+      fn: x => [x.ind.macd120.histo]
+    };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+
+    const features = [[0], [2.5], [5.0], [7.5], [10.0]];
+    const scaled = minMaxScaler.scaleInitial(features);
+
+    expect(scaled).toEqual([[-1], [-0.5], [0], [0.5], [1]]);
+  });
+
+  test("scaleInitial | macd | -100 -> 100", () => {
+    const featureSplit: FeatureSplit = {
+      name: "macd",
+      fn: x => [x.ind.macd120.histo]
+    };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+
+    const features = [[-100], [-80], [-50], [0], [60], [70], [100]];
+    const scaled = minMaxScaler.scaleInitial(features);
+
+    expect(scaled).toEqual([[-1], [-0.8], [-0.5], [0], [0.6], [0.7], [1]]);
+  });
+
+  test("scaleInitial | macd | 1 -> 5", () => {
+    const featureSplit: FeatureSplit = {
+      name: "macd",
+      fn: x => [x.ind.macd120.histo]
+    };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+
+    const features = [[1], [2], [3], [4], [5]];
+    const scaled = minMaxScaler.scaleInitial(features);
+
+    expect(scaled).toEqual([[-1], [-0.5], [0], [0.5], [1]]);
+  });
+
+  test("scaleInitial | macd | rsi", () => {
+    const featureSplit: FeatureSplit = {
+      name: "macd",
+      fn: x => [x.ind.macd120.histo, x.ind.rsi120x10]
+    };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+
+    const features = [[-100, 0], [-80, 25], [0, 50], [70, 75], [100, 100]];
+    const scaled = minMaxScaler.scaleInitial(features);
+
+    expect(scaled).toEqual([[-1, -1], [-0.8, -0.5], [0, 0], [0.7, 0.5], [1, 1]]);
+  });
+
+  test("scale | macd | rsi", () => {
+    const featureSplit: FeatureSplit = {
+      name: "macd",
+      fn: x => [x.ind.macd120.histo, x.ind.rsi120x10]
+    };
+    const minMaxScaler = new MinMaxScaler(featureSplit);
+
+    const features = [[-100, 0], [-80, 25], [0, 50], [70, 75], [100, 100]];
+    const scaled = minMaxScaler.scaleInitial(features);
+
+    // guard
+    expect(scaled).toEqual([[-1, -1], [-0.8, -0.5], [0, 0], [0.7, 0.5], [1, 1]]);
+
+    const test = [[-110, 0], [-10, 25], [0, 50], [90, 75], [120, 100]];
+    const testScaled = minMaxScaler.scale(test);
+
+    expect(round1Features(testScaled)).toEqual([[-1.1, -1], [-0.1, -0.5], [0, 0], [0.9, 0.5], [1.2, 1]]);
+  });
 });
 
-test("featureSplit | multiple", () => {
-  const featureSplit: FeatureSplit = {
-    name: "rsi_combo_macd",
-    fn: x => [x.ind.rsi30x10, x.ind.rsi60x10, x.ind.rsi120x10, x.ind.rsi240x10, x.ind.rsi480x10]
-  };
-  const minMaxScaler = new MinMaxScaler(featureSplit);
-  const indicators = minMaxScaler.getIndicatorNames();
-  expect(indicators).toEqual(["rsi30x10", "rsi60x10", "rsi120x10", "rsi240x10", "rsi480x10"]);
-});
-
-test("featureSplit | with point", () => {
-  const featureSplit: FeatureSplit = { name: "rsi_combo", fn: x => [x.ind.macd30.histo] };
-  const minMaxScaler = new MinMaxScaler(featureSplit);
-  const indicators = minMaxScaler.getIndicatorNames();
-  expect(indicators).toEqual(["macd30.histo"]);
-});
-
-test("featureSplit | comment", () => {
-  const featureSplit: FeatureSplit = {
-    name: "rsi_combo",
-    fn: x => [
-      x.ind.rsi30x10,
-      // x.ind.rsi60x10,
-      x.ind.rsi120x10
-    ]
-  };
-  const minMaxScaler = new MinMaxScaler(featureSplit);
-  const indicators = minMaxScaler.getIndicatorNames();
-  expect(indicators).toEqual(["rsi30x10", "rsi120x10"]);
-});
+// helper
+const round1Features = (features: number[][]) => {
+  return features.map(f => f.map(x => round1(x)));
+};
--- src/strat/features/MinMaxScaler.ts
+++ src/strat/features/MinMaxScaler.ts
@@ -1,14 +1,88 @@
 import { FeatureSplit } from ".";
+import { minBy, maxBy } from "lodash";
+import { rescale } from "../rescale";
+
+interface MinMax {
+  min: number;
+  max: number;
+}
+
+interface MinMaxMap {
+  [indicator: string]: MinMax;
+}
 
 export class MinMaxScaler {
   featureSplit: FeatureSplit;
+  indicators: string[];
+  minMaxMap: MinMaxMap;
 
   constructor(featureSplit: FeatureSplit) {
     this.featureSplit = featureSplit;
   }
 
+  // scale for training set, can only be called single time
+  scaleInitial(features: number[][]) {
+    if (this.indicators) {
+      throw new Error("ERROR: scaleInitial can only be called single time");
+    }
+
+    this.indicators = this.getIndicatorNames();
+
+    // sanity check
+    if (this.indicators.length !== features[0].length) {
+      throw new Error(
+        `ERROR: indicators.length (${this.indicators.length}) !== features.length (${features[0].length})`
+      );
+    }
+
+    this.minMaxMap = this.getMinMaxFeatureMap(features);
+    return this.scale(features);
+  }
+
+  // scale each next testing set, reusing existing minMaxMap
   scale(features: number[][]) {
-    console.log(this.featureSplit.fn.toString());
+    return features.map(f => {
+      return f.map((x, i) => {
+        const minMax = this.getMinMaxByIdx(i);
+        return rescale(x, minMax.min, minMax.max);
+      });
+    });
+  }
+
+  getMinMaxByIdx(idx: number) {
+    const indicator = this.indicators[idx];
+    return this.minMaxMap[indicator];
+  }
+
+  getMinMaxFeatureMap(features: number[][]) {
+    const fCount = features[0].length; // small count like 5
+    let minMaxMap: MinMaxMap = {};
+    for (let fc = 0; fc < fCount; fc++) {
+      // big length like 50k
+      const indicator = this.indicators[fc];
+
+      const hardcoded = this.getHardcodedMinMax(indicator);
+      if (hardcoded) {
+        minMaxMap[indicator] = hardcoded;
+      } else {
+        const min = minBy(features, x => x[fc])[fc];
+        const max = maxBy(features, x => x[fc])[fc];
+        minMaxMap[indicator] = { min, max };
+      }
+    }
+
+    return minMaxMap;
+  }
+
+  getHardcodedMinMax(indicator: string) {
+    const hardcoded_0_100 = ["rsi"];
+
+    for (let x of hardcoded_0_100) {
+      if (indicator.startsWith(x)) {
+        return { min: 0, max: 100 };
+      }
+    }
+    return null;
   }
 
   getIndicatorNames() {
