967379b14e05c5d280447ffde71a2fe834e76179
gb <guns.blazin.7@gmail.com>
2018-12-30 12:36:42 +0200

    ZerolagMACD more variations

--- src/strat/features/getZerolagMACD.ts
+++ src/strat/features/getZerolagMACD.ts
@@ -3,7 +3,7 @@ import { flatten } from "lodash";
 
 export const getZerolagMACD = (): FeatureSplit[] => {
   const timeframes = ["x30", "x60", "x120", "x240", "x480"];
-  const ps = ["sig9"];
+  const ps = ["sig5", "sig9", "sig2_10", "sig2_16"];
 
   return flatten(
     timeframes.map(tf => {
--- src/strat/indicators/ZerolagMACD.ts
+++ src/strat/indicators/ZerolagMACD.ts
@@ -3,19 +3,34 @@ import { Candle, MACD } from "../types";
 
 export interface IndZerolagMACD {
   [sig: string]: MACD;
+  sig5?: MACD;
   sig9?: MACD;
+  sig2_10?: MACD;
+  sig2_16?: MACD;
 }
 
 export class ZerolagMACD {
+  sig5: any;
   sig9: any;
+  sig2_10: any;
+  sig2_16: any;
 
   constructor(waveManager: WaveManager) {
+    // from here: https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_average_convergence_divergence_macd
+    this.sig5 = new XmBase(waveManager, () => new _ZerolagMACD({ short: 5, long: 35, signal: 5 }));
     this.sig9 = new XmBase(waveManager, () => new _ZerolagMACD({ short: 12, long: 26, signal: 9 }));
+
+    // http://etfhq.com/blog/2013/02/26/macd-test-results/
+    this.sig2_10 = new XmBase(waveManager, () => new _ZerolagMACD({ short: 10, long: 60, signal: 2 }));
+    this.sig2_16 = new XmBase(waveManager, () => new _ZerolagMACD({ short: 16, long: 97, signal: 2 }));
   }
 
   update(bigCandle: Candle): IndZerolagMACD {
     return {
-      sig9: this.sig9.update(bigCandle)
+      sig5: this.sig5.update(bigCandle),
+      sig9: this.sig9.update(bigCandle),
+      sig2_10: this.sig2_10.update(bigCandle),
+      sig2_16: this.sig2_16.update(bigCandle)
     };
   }
 }
