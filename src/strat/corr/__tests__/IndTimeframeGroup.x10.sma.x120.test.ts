76a3316a566ed5fce57fff0692e260acc5b8f175
gb <guns.blazin.7@gmail.com>
2019-02-03 16:19:19 +0200

    refactor some tests

--- src/strat/corr/__tests__/IndTimeframeGroup.x10.sma.x120.test.ts
+++ src/strat/corr/__tests__/IndTimeframeGroup.x10.sma.x120.test.ts
@@ -1,9 +1,9 @@
-import * as waveUtils from "./waveUtils";
-import { createCandle } from "./testUtils";
-import { SMA } from "../indicators/SMA";
-import { IndTimeframeGroup } from "../indicators/IndTimeframeGroup";
+import * as waveUtils from "../waveUtils";
+import { createCandle } from "../testUtils";
+import { SMA } from "../../indicators/SMA";
+import { IndTimeframeGroup } from "../../indicators/IndTimeframeGroup";
 
-describe("waveUtils", () => {
+describe("IndTimeframeGroup.x10.sma.x120", () => {
   const candles = [
     createCandle({ close: 0, start: 0, open: 1, high: 11, low: 11 }),
     createCandle({ close: 1, start: 1, open: 2, high: 12, low: 12 }),
@@ -17,50 +17,28 @@ describe("waveUtils", () => {
     createCandle({ close: 9, start: 9, open: 10, high: 20, low: 20 }),
     createCandle({ close: 10, start: 10, open: 10, high: 20, low: 20 }),
     createCandle({ close: 11, start: 11, open: 10, high: 20, low: 20 }),
-    createCandle({ close: 12, start: 12, open: 10, high: 20, low: 20 })
+    createCandle({ close: 12, start: 12, open: 10, high: 20, low: 20 }),
+    createCandle({ close: 13, start: 13, open: 10, high: 20, low: 20 })
   ];
 
   test("x1 -1", () => {
     const waveManagers = waveUtils.createManagers(10);
-    const sma = new IndTimeframeGroup(SMA, waveManagers, { x120: true }, {});
 
     let bigCandles = null;
-    let res = null;
+
     bigCandles = waveUtils.updateCandles(waveManagers, candles[0]);
-    res = sma.update(bigCandles);
-
     bigCandles = waveUtils.updateCandles(waveManagers, candles[1]);
-    res = sma.update(bigCandles);
-
     bigCandles = waveUtils.updateCandles(waveManagers, candles[2]);
-    res = sma.update(bigCandles);
-
     bigCandles = waveUtils.updateCandles(waveManagers, candles[3]);
-    res = sma.update(bigCandles);
-
     bigCandles = waveUtils.updateCandles(waveManagers, candles[4]);
-    res = sma.update(bigCandles);
-
     bigCandles = waveUtils.updateCandles(waveManagers, candles[5]);
-    res = sma.update(bigCandles);
-
     bigCandles = waveUtils.updateCandles(waveManagers, candles[6]);
-    res = sma.update(bigCandles);
-
     bigCandles = waveUtils.updateCandles(waveManagers, candles[7]);
-    res = sma.update(bigCandles);
-
     bigCandles = waveUtils.updateCandles(waveManagers, candles[8]);
-    res = sma.update(bigCandles);
-
     bigCandles = waveUtils.updateCandles(waveManagers, candles[9]);
-    res = sma.update(bigCandles);
-
     bigCandles = waveUtils.updateCandles(waveManagers, candles[10]);
-    res = sma.update(bigCandles);
 
     expect(bigCandles.x120).toBeNull();
-    expect(res.x120).toBeNull();
   });
 
   test("x1", () => {
@@ -109,9 +87,11 @@ describe("waveUtils", () => {
     bigCandles = waveUtils.updateCandles(waveManagers, candles[8]);
     bigCandles = waveUtils.updateCandles(waveManagers, candles[9]);
     bigCandles = waveUtils.updateCandles(waveManagers, candles[10]);
-    bigCandles = waveUtils.updateCandles(waveManagers, candles[11]);
-    bigCandles = waveUtils.updateCandles(waveManagers, candles[12]);
 
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[11]);
+    res = sma.update(bigCandles);
+
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[12]);
     res = sma.update(bigCandles);
 
     expect(bigCandles.x120).not.toBeNull();
@@ -119,7 +99,40 @@ describe("waveUtils", () => {
     expect(res.x120.p10).toEqual(12);
   });
 
-  test.only("x10 loop", () => {
+  test("x1 + 2", () => {
+    const waveManagers = waveUtils.createManagers(10);
+    const sma = new IndTimeframeGroup(SMA, waveManagers, { x120: true }, {});
+
+    let bigCandles = null;
+    let res = null;
+
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[0]);
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[1]);
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[2]);
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[3]);
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[4]);
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[5]);
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[6]);
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[7]);
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[8]);
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[9]);
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[10]);
+
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[11]);
+    res = sma.update(bigCandles);
+
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[12]);
+    res = sma.update(bigCandles);
+
+    bigCandles = waveUtils.updateCandles(waveManagers, candles[13]);
+    res = sma.update(bigCandles);
+
+    expect(bigCandles.x120).not.toBeNull();
+    expect(bigCandles.x120.close).toEqual(13);
+    expect(res.x120.p10).toEqual(13);
+  });
+
+  test("x10 loop", () => {
     const waveManagers = waveUtils.createManagers(10);
     const sma = new IndTimeframeGroup(SMA, waveManagers, { x120: true }, {});
 
---
+++ src/strat/corr/__tests__/waveUtils.updateCandles.test.ts
@@ -0,0 +1,241 @@
+import * as waveUtils from "../waveUtils";
+import { createCandle } from "../testUtils";
+import { WaveManagers } from "../../indicators/gekko";
+
+const candles = [
+  createCandle({ close: 0, start: 1, open: 1, high: 11, low: 11 }),
+  createCandle({ close: 1, start: 2, open: 2, high: 12, low: 12 }),
+  createCandle({ close: 2, start: 3, open: 3, high: 13, low: 13 }),
+  createCandle({ close: 3, start: 4, open: 4, high: 14, low: 14 }),
+  createCandle({ close: 4, start: 5, open: 5, high: 15, low: 15 }),
+  createCandle({ close: 5, start: 6, open: 6, high: 16, low: 16 }),
+  createCandle({ close: 6, start: 7, open: 7, high: 17, low: 17 }),
+  createCandle({ close: 7, start: 8, open: 8, high: 18, low: 18 }),
+  createCandle({ close: 8, start: 9, open: 9, high: 19, low: 19 }),
+  createCandle({ close: 9, start: 10, open: 10, high: 20, low: 20 }),
+  createCandle({ close: 10, start: 10, open: 10, high: 20, low: 20 }),
+  createCandle({ close: 11, start: 10, open: 10, high: 20, low: 20 }),
+  createCandle({ close: 12, start: 10, open: 10, high: 20, low: 20 }),
+  createCandle({ close: 13, start: 10, open: 10, high: 20, low: 20 })
+];
+
+describe("waveUtils.updateCandles x1 - 1", () => {
+  let waveManagers: WaveManagers = null;
+
+  beforeAll(() => {
+    waveManagers = waveUtils.createManagers(10);
+
+    waveUtils.updateCandles(waveManagers, candles[0]);
+    waveUtils.updateCandles(waveManagers, candles[1]);
+    waveUtils.updateCandles(waveManagers, candles[2]);
+    waveUtils.updateCandles(waveManagers, candles[3]);
+    waveUtils.updateCandles(waveManagers, candles[4]);
+    waveUtils.updateCandles(waveManagers, candles[5]);
+    waveUtils.updateCandles(waveManagers, candles[6]);
+    waveUtils.updateCandles(waveManagers, candles[7]);
+    waveUtils.updateCandles(waveManagers, candles[8]);
+    waveUtils.updateCandles(waveManagers, candles[9]);
+    waveUtils.updateCandles(waveManagers, candles[10]);
+    // waveUtils.updateCandles(waveManagers, candles[11]);
+  });
+
+  test("wave 0", () => {
+    const candles = waveManagers.x120.getWaveCandles(0);
+    expect(candles.map(x => x.close)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, undefined]);
+  });
+
+  test("wave 1", () => {
+    const candles = waveManagers.x120.getWaveCandles(1);
+    expect(candles.map(x => x.close)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, undefined, undefined]);
+  });
+
+  test("wave 2", () => {
+    const candles = waveManagers.x120.getWaveCandles(2);
+    expect(candles.map(x => x.close)).toEqual([2, 3, 4, 5, 6, 7, 8, 9, 10, undefined, undefined, undefined]);
+  });
+
+  test("wave 9", () => {
+    const candles = waveManagers.x120.getWaveCandles(9);
+    expect(candles.map(x => x.close)).toEqual([
+      9,
+      10,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    ]);
+  });
+
+  test("wave 10", () => {
+    const candles = waveManagers.x120.getWaveCandles(10);
+    expect(candles.map(x => x.close)).toEqual([
+      10,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    ]);
+  });
+
+  test("wave 11", () => {
+    const candles = waveManagers.x120.getWaveCandles(11);
+    expect(candles.map(x => x.close)).toEqual([
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    ]);
+  });
+});
+
+describe("waveUtils.updateCandles x1 2", () => {
+  let waveManagers: WaveManagers = null;
+
+  beforeAll(() => {
+    waveManagers = waveUtils.createManagers(10);
+
+    waveUtils.updateCandles(waveManagers, candles[0]);
+    waveUtils.updateCandles(waveManagers, candles[1]);
+    waveUtils.updateCandles(waveManagers, candles[2]);
+    waveUtils.updateCandles(waveManagers, candles[3]);
+    waveUtils.updateCandles(waveManagers, candles[4]);
+    waveUtils.updateCandles(waveManagers, candles[5]);
+    waveUtils.updateCandles(waveManagers, candles[6]);
+    waveUtils.updateCandles(waveManagers, candles[7]);
+    waveUtils.updateCandles(waveManagers, candles[8]);
+    waveUtils.updateCandles(waveManagers, candles[9]);
+    waveUtils.updateCandles(waveManagers, candles[10]);
+    waveUtils.updateCandles(waveManagers, candles[11]);
+    waveUtils.updateCandles(waveManagers, candles[12]);
+    waveUtils.updateCandles(waveManagers, candles[13]);
+  });
+
+  test("wave 0", () => {
+    const candles = waveManagers.x120.getWaveCandles(0);
+    expect(candles.map(x => x.close)).toEqual([
+      12,
+      13,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    ]);
+  });
+
+  test("wave 1", () => {
+    const candles = waveManagers.x120.getWaveCandles(1);
+    expect(candles.map(x => x.close)).toEqual([
+      13,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    ]);
+  });
+
+  test("wave 2", () => {
+    const candles = waveManagers.x120.getWaveCandles(2);
+    expect(candles.map(x => x.close)).toEqual([
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    ]);
+  });
+
+  test("wave 9", () => {
+    const candles = waveManagers.x120.getWaveCandles(9);
+    expect(candles.map(x => x.close)).toEqual([
+      9,
+      10,
+      11,
+      12,
+      13,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    ]);
+  });
+
+  test("wave 10", () => {
+    const candles = waveManagers.x120.getWaveCandles(10);
+    expect(candles.map(x => x.close)).toEqual([
+      10,
+      11,
+      12,
+      13,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    ]);
+  });
+
+  test("wave 11", () => {
+    const candles = waveManagers.x120.getWaveCandles(11);
+    expect(candles.map(x => x.close)).toEqual([
+      11,
+      12,
+      13,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    ]);
+  });
+});
